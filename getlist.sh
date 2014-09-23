47f9b5db07baf301fb32056f8b240f9ee454ce6e
Juan Luis Baptiste <juan.baptiste@gmail.com>
2014-09-22 19:21:16 -0500

    Permitir especificar el nombre del archivo a procesar por linea de comandos

--- getlist.sh
+++ getlist.sh
@@ -1,8 +1,30 @@
 rm -f listado_pinf_ult.csv
 rm -f listado_pinf_ult.zip
-wget --user=netclean1212 --password=d5029ab925 http://www.ixp.net.co/Filter/listado_pinf_ult.zip
-unzip -P CCIT2014 listado_pinf_ult.zip
-iconv -f ISO-8859-1 -t utf-8 listado_pinf_ult.csv > IXP.raw.utf8
+rm -f IXP.*
+
+archivo_descarga="listado_pinf_ult.zip"
+archivo="$1"
+
+if [ "$archivo" == "" ]
+then
+    echo -e "No se ha especificado el archivo, descargando...\n"
+    wget --user=netclean1212 --password=d5029ab925 http://www.ixp.net.co/Filter/$archivo_descarga
+    unzip -P CCIT2014 $archivo_descarga
+    archivo=$archivo_descarga
+    #split -d -l 1000 $archivo mintic_ --additional-suffix=.txt    
+fi    
+
+extension="${filename##*.}"
+
+if [  $extension == "csv" ]
+then
+    archivo_csv="`basename $archivo zip`csv"
+else
+    archivo_csv=$archivo
+fi    
+
+iconv -f ISO-8859-1 -t utf-8 $archivo_csv > IXP.raw.utf8
+
 sort IXP.raw.utf8 | uniq | tee IXP.raw.utf8.sorted
 python parser.py
 sort IXP.raw.utf8.sorted.ok | uniq | tee IXP.txt
