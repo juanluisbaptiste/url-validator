45d8c460b87f4e2bc3763e802775a354e48c6c54
Juan Luis Baptiste <juan.baptiste@gmail.com>
2014-10-02 22:40:14 -0500

    Identation fixes.

--- nuevo/urlvalidator.py
+++ nuevo/urlvalidator.py
@@ -30,18 +30,29 @@ def isDomainNameValid ( name ):
 		return False
 
 def isURLValid(url) :
-  regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-  if regex.match(url):
-    return True
-  else:
-    return False
+	#regex = re.compile(
+		#r'^(?:http|ftp)s?://' # http:// or https://
+		#r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+		#r'localhost|' #localhost...
+		#r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+		#r'(?::\d+)?' # optional port
+		#r'(?:/?|[/?]\S+)$', re.IGNORECASE)	
+	regex = re.compile(
+        r'^(?:[a-z0-9\.\-]*)://'  # scheme is validated separately
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}(?<!-)\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)        
+	if regex.match(url):
+		return True
+	else:
+		return False
    
+def isPathValid(strg, search=re.compile(r'^\/[/.a-zA-Z0-9-~_+:%=;,!]*$').search):
+	return bool(search(strg))    
+
 
 def openFile(filename, mode = 'r'):
 #	if not mode:
