26e86feaaafd45cef2b0d78205dadb3bb6c394d4
Juan Luis Baptiste <juan.baptiste@gmail.com>
2014-10-11 20:12:24 -0500

    Documentation.

--- nuevo/urlvalidator.py
+++ nuevo/urlvalidator.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+"""
+URL validation tool. 
+It receives a file with url's to discard malformed url's 
+from valid ones.
+
+If the --test-urls parameter is specified then the non-malformed
+url's will be tested to see if they don't have any connection 
+errors or return HTTP error codes.
+"""
 import argparse
 import os
 import re
@@ -26,12 +35,42 @@ processed_urls_counter = 0
 verboseprint = None
 
 def signal_handler(signal, frame):
+    """
+    Ctr-C signal handler function to be able to stop
+    a running test.
+
+    Parameters
+    ----------
+
+    signal : signal
+        The signal the process received.
+    frame : handler
+        function to call when the signal is received to 
+        process it.
+    """
     print "\nUser pressed Ctrl-C, stopping..."
     stopTest()
     sys.exit(0)
+#Connects the SIGINT signal to our handler function to process it    
 signal.signal(signal.SIGINT, signal_handler)
     
 def isDomainNameValid ( name ):
+    """
+    Checks if a domain name is valid (ie: www.python.org)
+
+    Parameters
+    ----------
+
+    name : string 
+        Domain to verify
+
+    Returns
+    -------
+    bool
+        Returns True if the domain matches the regex 
+        (is valid), False otherwise.
+
+    """
     # TODO: Works but accepts hostnames with a name of at least 3 characters with no domain. ie. www instead of www.test.com
     regex = re.compile(r'[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,5}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?|'
         r'(([2]([0-4][0-9]|[5][0-5])|[0-1]?[0-9]?[0-9])[.]){3}(([2]([0-4][0-9]|[5][0-5])|[0-1]?[0-9]?[0-9]))', re.IGNORECASE)
@@ -42,6 +81,22 @@ def isDomainNameValid ( name ):
         return False
 
 def isURLValid(url) :
+    """
+    Checks if a URL is valid (ie: http://www.python.org/doc/)
+
+    Parameters
+    ----------
+
+    url : string 
+        URL to verify
+
+    Returns
+    -------
+    bool
+        Returns True if the url matches the regex 
+        (is valid), False otherwise.
+
+    """
     regex = re.compile(
         r'^(?:[a-z0-9\.\-]*)://'  # scheme is validated separately
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}(?<!-)\.?)|'  # domain...
@@ -56,10 +111,44 @@ def isURLValid(url) :
         return False
 
 def isPathValid(strg, start=re.compile(r'^\/[/.a-zA-Z0-9-~_+:%=;,!]*$').search):
+    """
+    Checks if the path part of an url is valid (ie: /foo)
+
+    Parameters
+    ----------
+
+    strg : string 
+        URL path to verify
+    start : regex 
+        Regular expression to use to test the path
+    Returns
+    -------
+    bool
+        Returns True if the path matches the regex 
+        (is valid), False otherwise.
+
+    """
     return bool(start(strg))
 
 
 def openFile(filename, mode = 'r'):
+    """
+    Opens a file. 
+
+    Parameters
+    ----------
+
+    filename : string 
+        Path to the file to open.    
+    mode : string
+        mode to open the file, read-only by default.
+
+    Returns
+    -------
+    Array_like
+        Returns the file contents.
+
+    """
     if os.path.exists(filename):
         with open(filename) as urls:
         #urls = open(filename, mode)
@@ -72,13 +161,34 @@ def openFile(filename, mode = 'r'):
 
 
 def saveFile(filename,content):
-        arch = open(filename, 'w')
-        try:
-            arch.write(content)
-        finally:
-            arch.close()
+    """
+    Saves a file. 
+
+    Parameters
+    ----------
+
+    filename : string 
+        Path to the file to save.    
+    mode : string
+        Contents to save to the file.
+
+    """
+    arch = open(filename, 'w')
+    try:
+        arch.write(content)
+    finally:
+        arch.close()
 
 def testUrls():
+    """
+    Connects to each of the non-malformed url's to see if they are 
+    valid. Valid means we can connect to them and they return a 
+    HTTP code < 400. Any other type of error (DNS failure, connect 
+    timeout, HTTP codes >= 400) will move the url to the 
+    invalids list.
+
+    The connections are done asynchronically using the Tornado library.
+    """    
     global concurrent, request_timeout, connect_timeout, request
     #Use the curl implementation for better performance
 
@@ -96,10 +206,30 @@ def testUrls():
 
 
 def stopTest():
+    """
+    Stop the tornado IOLoop to finish the tests.
+
+    Returns
+    -------
+    bool
+        Returns True if the loop was stopped successfully, 
+        False otherwise.
+    """
     verboseprint("Finishing off...\n")
     return tornado.ioloop.IOLoop.instance().stop()
 
 def handle_request(response):
+    """
+    Handles an HTTP response from one of the async 
+    connections. 
+
+    Parameters
+    ----------
+
+    response : HTTPResponse 
+        HTTP response object.    
+    
+    """    
     global url_counter, processed_urls_counter, valid_urls_counter, invalid_urls_counter
 
     #Handle error responses (DNS failures, timeouts, etc)
@@ -127,6 +257,16 @@ def handle_request(response):
         stopTest()
 
 def writeInvalidFile(filename):
+    """
+    Saves the file with the invalid url's. 
+
+    Parameters
+    ----------
+
+    filename : string 
+        Path to the file to save.
+
+    """    
     global invalid_urls
     content=""
 
@@ -135,6 +275,16 @@ def writeInvalidFile(filename):
     saveFile(filename, content)
 
 def writeValidFile(filename):
+    """
+    Saves the file with the valid url's. 
+
+    Parameters
+    ----------
+
+    filename : string 
+        Path to the file to save.    
+
+    """    
     global valid_urls
     content=""
 
@@ -143,6 +293,17 @@ def writeValidFile(filename):
     saveFile(filename, content)
 
 def parseFile(filename):
+    """
+    Parses the input file separating the malformed url's 
+    from the non-malformed ones. 
+
+    Parameters
+    ----------
+
+    filename : string 
+        Path to the file to open.    
+
+    """    
     global url_counter,original_urls ,invalid_urls, fixed_url_counter, valid_urls_counter, invalid_urls_counter
     original_urls = openFile(filename)
     schemes = ['http', 'https']
@@ -182,6 +343,20 @@ def parseFile(filename):
 
 
 def start(args):
+    """
+    Starts the validation process. First it will look 
+    for malformed url's. Then, if the --test-urls 
+    parameter is used it will try to connect to each of 
+    the valid url's and mark as invalid the ones with
+    errors.  
+
+    Parameters
+    ----------
+
+    args : *args 
+        Command line parameters    
+
+    """
     global url_counter,fixed_url_counter, concurrent, verboseprint, invalid_urls_counter
 
     if args.verbose:
@@ -225,6 +400,18 @@ def start(args):
     writeValidFile(args.dest_file[0])
 
 def parsingFileStats(args):
+    """
+    Prints some stats about the results found after 
+    parsing the file contents looking for malformed 
+    url's.
+
+    Parameters
+    ----------
+
+    args : *args 
+        Command line parameters    
+
+    """
     print "Input file: " + args.source_file[0]
     print "Output file: " + args.dest_file[0]
     print "Invalid url's file: " + args.invalid_file[0] + "\n"
@@ -236,12 +423,23 @@ def parsingFileStats(args):
 
 
 def testUrlsStats():
+    """
+    Prints some stats about the results found after 
+    testing all of the non-malformed url's looking 
+    for errors.
+
+    """
     print "\nFound " + `invalid_urls_counter` + " invalid url's"
     print "\nResults:\n"
     print "Valid url's: " + `len(valid_urls)`
     print "Total invalid url's: " + `len(invalid_urls)`
 
 def run():
+    """
+    Main function, handles the command line parameters 
+    and calls the start() function to process them and 
+    start the tests.
+    """
     global concurrent
     # create the top-level parser
     parser = argparse.ArgumentParser(description='Apukay Security URL Validator Tool.',
